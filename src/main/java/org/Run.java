c14d7502efb3174e425941eefe2d85fffb8e6ba2
shuo.zhao <shuo.zhao@ughen.com>
2018-12-05 17:33:24 +0800

    add frame in run edit compareZONE

--- src/main/java/org/Run.java
+++ src/main/java/org/Run.java
@@ -1,17 +1,22 @@
 package org;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Date;
 
+import act.A02;
 import util.ClickPointRandom;
 import util.CompareZone;
+import util.NewFrame;
+
+import javax.swing.*;
 
 public class Run {
 public static void main(String[] args) {
 
 		//System.out.println(ClickPointRandom.RandomClick(10, 30));;
         System.out.println(new Date());
-	    CompareZone compareZone=new CompareZone();
-	    boolean flag=compareZone.toCompareZone("D:/key.png");
-	    System.out.println(flag);
-	    System.out.println(new Date());
+	   NewFrame frame=new NewFrame();
+	   frame.NewFrame();
+
  }
 }
--- src/main/java/org/WindowEdit.java
+++ src/main/java/org/WindowEdit.java
@@ -4,13 +4,15 @@ import com.sun.jna.platform.win32.User32;
 import com.sun.jna.platform.win32.WinDef;
 import com.sun.jna.platform.win32.WinDef.HWND;
 import com.sun.jna.platform.win32.WinUser;
+import util.Zone;
+
 /**
  * Created by lenovo on 2017/4/27.
  * 使用winID来获得窗口的类型和标题，然后发送消息或者其他操作
  *
  */
 public class WindowEdit {
-    public static void main(String[] args) {
+    public static void WindowEdit() {
 
 //        HWND hwnd = User32.INSTANCE.FindWindow
 //                (null, "TIM"); // 第一个参数是Windows窗体的窗体类，第二个参数是窗体的标题。不熟悉windows编程的需要先找一些Windows窗体数据结构的知识来看看，还有windows消息循环处理，其他的东西不用看太多。
@@ -25,7 +27,7 @@ public class WindowEdit {
             //User32.INSTANCE.GetForegroundWindow() //获取现在前台窗口
             WinDef.RECT qqwin_rect = new  WinDef.RECT();
             User32.INSTANCE.GetWindowRect(hwnd, qqwin_rect);
-            User32.INSTANCE.MoveWindow(hwnd, 0, 0, 400, 300, true);
+            User32.INSTANCE.MoveWindow(hwnd, 0, 0, Zone.x, Zone.y, true);
             int qqwin_width = qqwin_rect.right-qqwin_rect.left;
             int qqwin_height = qqwin_rect.bottom-qqwin_rect.top;
 
